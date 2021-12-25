69ce100f4b16275b2d43bb38a0b4479a40568acc
Alexander Garuba <alexandergaruba96@gmail.com>
2021-12-25 01:40:01 +0100

    CRUD with json-server completed

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -28,22 +28,34 @@ export const FeedbackProvider = ({ children }) => {
       body: JSON.stringify(newFeedback),
     });
     const data = await response.json();
+
     setFeedback([data, ...feedback]);
   };
 
   // Delete Feedback
-  const deleteFeedback = (id) => {
+  const deleteFeedback = async (id) => {
     if (window.confirm("Are you sure you want to delete")) {
+      await fetch(`/feedback/${id}`, {
+        method: "DELETE",
+      });
+
       setFeedback(feedback.filter((item) => item.id !== id));
     }
   };
 
   // Update feedback item
-  const updateFeedback = (id, updateItem) => {
+  const updateFeedback = async (id, updateItem) => {
+    const response = await fetch(`/feedback/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(updateItem),
+    });
+    const data = await response.json();
+
     setFeedback(
-      feedback.map((item) =>
-        item.id === id ? { ...item, ...updateItem } : item
-      )
+      feedback.map((item) => (item.id === id ? { ...item, ...data } : item))
     );
   };
 
